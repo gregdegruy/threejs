1dedf6e12b7c513fe3580b564179758fdfdf05fc
Greg <gregory.degruy@gmail.com>
2021-05-08 13:38:21 -0700

    ~ icy cube time

--- threejsfundamentals/main.js
+++ threejsfundamentals/main.js
@@ -39,8 +39,8 @@ function main() {
     p = min(max(p * 3.0 - 1.8, 0.1), 2.0);
     
     vec2 r = mod(uv * float(TILES), 1.0);
-    r = vec2(pow(r.x - 0.5, 2.0), pow(r.y - 0.5, 2.0));
-    p *= 1.0 - pow(min(1.0, 12.0 * dot(r, r)), 2.0);
+    r = vec2(pow(r.x - 0.3, 1.0), pow(r.y - 0.3, 1.0));
+    p *= 1.0 - pow(min(0.1, 0.1 * dot(r, r)), 0.1);
     
     fragColor = vec4(COLOR, 1.0) * p;
   }
