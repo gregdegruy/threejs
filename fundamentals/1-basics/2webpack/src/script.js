f77f1547f23dc2ff7af8bdd613a5554736f2a3b6
Greg <gregory.degruy@gmail.com>
2021-05-16 15:08:28 -0700

    + axes helper of len 4

--- fundamentals/1-basics/2webpack/src/script.js
+++ fundamentals/1-basics/2webpack/src/script.js
@@ -5,7 +5,7 @@ const scene = new THREE.Scene()
 const geometry = new THREE.BoxGeometry(1,1,1)
 const material = new THREE.MeshBasicMaterial({color: 0x00aaff})
 const cube = new THREE.Mesh(geometry, material)
-cube.position.set(2, -0.6, -3)
+cube.position.set(-2, -1, -0.5)
 scene.add(cube)
 // cube.position.normalize()
 console.log("Cube len set to 1 with normalization: " + cube.position.length())
@@ -17,9 +17,13 @@ const sizes = {
 }
 const aspectRatio = sizes.width / sizes.height
 const camera = new THREE.PerspectiveCamera(fov, aspectRatio)
-camera.position.z = 3
+camera.position.set(0.5, 0.5, 5)
 scene.add(camera)
 console.log("Distance between camera and cube: " + cube.position.distanceTo(camera.position))
+
+const axesLength = 4
+const axesHelper = new THREE.AxesHelper(axesLength);
+scene.add(axesHelper)
 
 const canvas = document.getElementById("universe-canvas");
 const renderer = new THREE.WebGLRenderer({
