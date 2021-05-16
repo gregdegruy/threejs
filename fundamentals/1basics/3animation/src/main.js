6a4bae1a76dc7c3c803d29ad0294731f32a780f5
Greg <gregory.degruy@gmail.com>
2021-05-16 16:42:42 -0700

    + class to handle more complex graphics

--- fundamentals/1basics/3animation/src/main.js
+++ fundamentals/1basics/3animation/src/main.js
@@ -1,49 +1,5 @@
-import './style.css'
-import * as THREE from 'three'
+import "./style.css"
+import Scene from "./gl/scene";
 
-const scene = new THREE.Scene()
-const geometry = new THREE.BoxGeometry(1,1,1)
-const blueMaterial = new THREE.MeshBasicMaterial({ color: 0x00aaff})
-const blueCube = new THREE.Mesh(geometry, blueMaterial)
-blueCube.position.set(-2, -1, -0.5)
-
-const fov = 75
-const screen = {
-    width: 800,
-    height: 600
-}
-const aspectRatio = screen.width / screen.height
-const camera = new THREE.PerspectiveCamera(fov, aspectRatio)
-camera.position.set(0.5, 0.5, 3.5)
-
-const axesLength = 4
-const axesHelper = new THREE.AxesHelper(axesLength);
-// camera.lookAt(axesHelper.position)
-
-const group = new THREE.Group()
-const redCube = new THREE.Mesh(
-    geometry,
-    new THREE.MeshBasicMaterial({ color: 0xff6961 })
-)
-redCube.position.z = -3
-const greenCube = new THREE.Mesh(
-    geometry,
-    new THREE.MeshBasicMaterial({ color: 0x77dd77 })
-)
-greenCube.scale.set(0.7, 0.7, 0.7)
-greenCube.position.z = 1
-group.add(redCube)
-group.add(greenCube)
-group.position.x = 1.5
-group.rotation.z = 0.4
-
-scene.add(blueCube)
-scene.add(camera)
-scene.add(axesHelper)
-scene.add(group)
-const canvas = document.getElementById("universe-canvas");
-const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
-})
-renderer.setSize(screen.width, screen.height)
-renderer.render(scene, camera)
+const scene = new Scene();
+scene.init();
